95e791068593a6ef05a574d4731fa5e77d780447
fciuculescu <fciuculescu@deloittece.com>
2020-08-26 20:02:30 +0300

    used Qualifier: -created 3 different Services, each for every controller.

--- src/main/java/com/example/springdi/services/SetterInjectedGreetingService.java
+++ src/main/java/com/example/springdi/services/SetterInjectedGreetingService.java
@@ -3,9 +3,10 @@ package com.example.springdi.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GreetingServiceImpl implements GreetingService {
+public class SetterInjectedGreetingService implements GreetingService {
+
     @Override
     public String sayGreeting() {
-        return "Hello World";
+        return "Hello World --> Setter";
     }
 }
