f9e008f68313600df9544e2c8b17f7eacd085c69
fciuculescu <fciuculescu@deloittece.com>
2020-08-26 18:21:48 +0300

    used DI using Spring Framework

--- src/main/java/com/example/springdi/SpringdiApplication.java
+++ src/main/java/com/example/springdi/SpringdiApplication.java
@@ -1,6 +1,9 @@
 package com.example.springdi;
 
+import com.example.springdi.controllers.ConstructorInjectedController;
 import com.example.springdi.controllers.MyController;
+import com.example.springdi.controllers.PropertyInjectedController;
+import com.example.springdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +18,24 @@ public class SpringdiApplication {
 
         String greeting = myController.sayHello();
         System.out.println(greeting);
+
+        System.out.println("------- Property");
+
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+        System.out.println(propertyInjectedController.getGreeting());
+
+        System.out.println("------- Setter");
+
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+        System.out.println(setterInjectedController.getGreeting());
+
+        System.out.println("------- Constructor");
+
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+        System.out.println(constructorInjectedController.getGreeting());
     }
 
 }
--- src/main/java/com/example/springdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/example/springdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package com.example.springdi.controllers;
 
 import com.example.springdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
