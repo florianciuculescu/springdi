f4caf6c67e4e0162e746320dc8e20a98603ba9cd
fciuculescu <fciuculescu@deloittece.com>
2020-08-26 20:34:33 +0300

    used default profile

--- src/main/java/com/example/springdi/services/I18nSpanishGreetingService.java
+++ src/main/java/com/example/springdi/services/I18nSpanishGreetingService.java
@@ -2,7 +2,8 @@ package com.example.springdi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("ES")
+
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
 
