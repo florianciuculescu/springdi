afbf6f3f732ccaeddf69dbe02b37a4d7ec0c6092
fciuculescu <fciuculescu@deloittece.com>
2020-08-26 16:33:07 +0300

    retrieved a specific bean out of ApplicationContext

--- src/main/java/com/example/springdi/SpringdiApplication.java
+++ src/main/java/com/example/springdi/SpringdiApplication.java
@@ -1,13 +1,20 @@
 package com.example.springdi;
 
+import com.example.springdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringdiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringdiApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SpringdiApplication.class, args);
+
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        String greeting = myController.sayHello();
+        System.out.println(greeting);
     }
 
 }
